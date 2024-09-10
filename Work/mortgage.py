9ce68d97f550a52cc88bdd93eb97311d5232b50c
Ben Weintraub <benweint@gmail.com>
2024-09-09 21:51:53 -0700

    implement exercise 2 (mortgage payment)

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,30 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000.0
+
+while principal > 0:
+	if principal < payment:
+		principal = 0
+		total_paid += principal
+	else:
+		principal = principal * (1 + rate/12) - payment
+		total_paid += payment
+
+		if month >= extra_payment_start_month and month <= extra_payment_end_month:
+			principal -= extra_payment
+			total_paid += extra_payment
+
+	month += 1
+	print(f'{month} {total_paid:.2f} {principal:0.2f}')
+
+print(f'Total paid ${total_paid:.2f} over {month} months')
