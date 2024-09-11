d2e93d6af888e0b808fd3374d3d08548c27f8762
Ben Weintraub <benweint@gmail.com>
2024-09-11 15:35:45 -0700

    flesh out fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,37 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
+	'''
+	Parse a CSV file into a list of records
+	'''
+	with open(filename) as f:
+		rows = csv.reader(f, delimiter=delimiter)
+
+		if has_headers:
+			headers = next(rows)
+		else:
+			headers = None
+
+		records = []
+		for row in rows:
+			if not row: # Skip rows with no data
+				continue
+
+			if types:
+				row = [f(x) for f, x in zip(types, row)]
+
+			if select:
+				# TODO: select + has_headers=False is invalid!
+				record = { k: v for k, v in zip(headers, row) if k in select }
+			elif headers:
+				record = dict(zip(headers, row))
+			else:
+				record = tuple(row)
+
+			records.append(record)
+
+	return records
