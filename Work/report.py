894bb0bb0b2f65bddd190a41d0d6935216e00ae5
Ben Weintraub <benweint@gmail.com>
2024-09-11 10:55:43 -0700

    exercise 2.11: formatting tweaks

--- Work/report.py
+++ Work/report.py
@@ -50,7 +50,9 @@ portfolio = read_portfolio('Data/portfolio.csv')
 prices = read_prices('Data/prices.csv')
 report = make_report(portfolio, prices)
 
-print('Name       Shares     Price      Change    ')
-print('---------- ---------- ---------- ----------')
+headers = ('Name', 'Shares', 'Price', 'Change')
+print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+print(' '.join(['-' * 10] * len(headers)))
+
 for name, shares, price, change in report:
 	print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
