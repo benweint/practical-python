14bad3ddc7390d7c5f9895cbeeb2157c40a8939e
Ben Weintraub <benweint@gmail.com>
2024-09-11 14:40:39 -0700

    exercise 3.1: function re-org

--- Work/report.py
+++ Work/report.py
@@ -40,14 +40,16 @@ def make_report(portfolio, prices):
 	return result
 
 
+def print_report(report):
+	headers = ('Name', 'Shares', 'Price', 'Change')
+	print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+	print(' '.join(['-' * 10] * len(headers)))
+
+	for name, shares, price, change in report:
+		formatted_price = f'${price:.2f}'
+		print(f'{name:>10s} {shares:>10d} ${formatted_price:>10s} {change:>10.2f}')
+
 portfolio = read_portfolio('Data/portfolio.csv')
 prices = read_prices('Data/prices.csv')
 report = make_report(portfolio, prices)
-
-headers = ('Name', 'Shares', 'Price', 'Change')
-print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
-print(' '.join(['-' * 10] * len(headers)))
-
-for name, shares, price, change in report:
-	formatted_price = f'${price:.2f}'
-	print(f'{name:>10s} {shares:>10d} ${formatted_price:>10s} {change:>10.2f}')
+print_report(report)
