dd4e93c5fa4c44991b6f9be086406d791b4a08e5
Ben Weintraub <benweint@gmail.com>
2024-09-11 09:05:43 -0700

    exercise 1.30: add missing value handling to pcost

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,18 +2,23 @@
 #
 # Exercise 1.27
 
-path = 'Data/portfolio.csv'
-total_cost = 0
+def portfolio_cost(filename):
+	total_cost = 0
+	with open(filename, "rt") as f:
+		headers = next(f).strip().split(",")
+		shares_idx = headers.index('shares')
+		price_idx = headers.index('price')
+		for line in f:
+			cols = line.split(",")
+			try:
+				shares = int(cols[shares_idx])
+				price = float(cols[price_idx])
+			except ValueError as e:
+				print(f'Skipping line {line} due to error: {e}')
+			cost = price * shares
+			total_cost += cost
+	return total_cost
 
-with open(path, "rt") as f:
-	headers = next(f).strip().split(",")
-	shares_idx = headers.index('shares')
-	price_idx = headers.index('price')
-	for line in f:
-		cols = line.split(",")
-		shares = int(cols[shares_idx])
-		price = float(cols[price_idx])
-		cost = price * shares
-		total_cost += cost
-
+total_cost = portfolio_cost('Data/portfolio.csv')
 print(f'Total cost {total_cost:.2f}')
+
