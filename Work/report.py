252fbd37bf719e022825cea4f0d189f57363b7ec
Ben Weintraub <benweint@gmail.com>
2024-09-11 19:45:32 -0700

    exercise 4.6: swappable formatters

--- Work/report.py
+++ Work/report.py
@@ -4,6 +4,7 @@
 
 import csv
 import fileparse
+import tableformat
 from stock import Stock
 
 def read_portfolio(filename):
@@ -26,21 +27,23 @@ def make_report(portfolio, prices):
 	return result
 
 
-def print_report(report):
-	headers = ('Name', 'Shares', 'Price', 'Change')
-	print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
-	print(' '.join(['-' * 10] * len(headers)))
+def print_report(report, formatter):
+	formatter.headings(['Name', 'Shares', 'Price', 'Change'])
+	# print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+	# print(' '.join(['-' * 10] * len(headers)))
 
 	for name, shares, price, change in report:
 		formatted_price = f'${price:.2f}'
-		print(f'{name:>10s} {shares:>10d} {formatted_price:>10s} {change:>10.2f}')
+		rowdata = [name, str(shares), formatted_price, f'{change:.2f}']
+		formatter.row(rowdata)
 
 
 def portfolio_report(portfolio_path, prices_path):
 	portfolio = read_portfolio(portfolio_path)
 	prices = read_prices(prices_path)
 	report = make_report(portfolio, prices)
-	print_report(report)
+	formatter = tableformat.TextTableFormatter()
+	print_report(report, formatter)
 
 
 def main(args):
