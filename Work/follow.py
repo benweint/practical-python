717e2c6d222c535395fefadfdc332ae9d8d7f814
Ben Weintraub <benweint@gmail.com>
2024-09-12 08:46:06 -0700

    exercise 6.6: watching your portfolio

--- Work/follow.py
+++ Work/follow.py
@@ -12,10 +12,16 @@ def follow(filename):
 		yield line
 
 
-for line in follow('Data/stocklog.csv'):
-	fields = line.split(',')
-	name = fields[0].strip('"')
-	price = float(fields[1])
-	change = float(fields[4])
-	if change < 0:
-		print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+if __name__ == '__main__':
+	import report
+
+	portfolio = report.read_portfolio('Data/portfolio.csv')
+
+	for line in follow('Data/stocklog.csv'):
+		fields = line.split(',')
+		name = fields[0].strip('"')
+		if name in portfolio:
+			price = float(fields[1])
+			change = float(fields[4])
+			print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+
