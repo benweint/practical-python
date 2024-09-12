d59f202d1d1a181925b0038ac84e9eaa73c984a1
Ben Weintraub <benweint@gmail.com>
2024-09-12 09:47:58 -0700

    exercise 6.15: use more generators

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,41 +1,28 @@
 from follow import follow
 import csv
+import tableformat
 
 
 def parse_stock_data(lines):
 	rows = csv.reader(lines)
-	rows = select_columns(rows, [0, 1, 4])
-	rows = convert_types(rows, [str, float, float])
-	rows = make_dicts(rows, ['name', 'price', 'change'])
+	rows = ([row[0], row[1], row[4]] for row in rows)
+	rows = ([x[0], float(x[1]), float(x[2])] for x in rows)
+	rows = ({ 'name': x[0], 'price': x[1], 'change': x[2] } for x in rows)
 	return rows
 
 
-def select_columns(rows, indices):
-	for row in rows:
-		yield [row[index] for index in indices]
-
-
-def convert_types(rows, types):
-	for row in rows:
-		yield [func(val) for func, val in zip(types, row)]
-
-
-def make_dicts(rows, headers):
-	for row in rows:
-		yield dict(zip(headers, row))
-
-
-def filter_symbols(rows, names):
-	for row in rows:
-		if row['name'] in names:
-			yield row
-
-
-if __name__ == '__main__':
+def ticker(portfile, logfile, fmt):
 	import report
 	portfolio = report.read_portfolio('Data/portfolio.csv')
 	rows = parse_stock_data(follow('Data/stocklog.csv'))
-	rows = filter_symbols(rows, portfolio)
-	for row in rows:
-		print(row)
+	rows = (row for row in rows if row['name'] in portfolio)
 
+	formatter = tableformat.create_formatter(fmt)
+	formatter.headings(['Name', 'Price', 'Change'])
+
+	for row in rows:
+		formatter.row([row['name'], f'${row["price"]:.2f}', str(row['change'])])
+
+
+if __name__ == '__main__':
+	ticker('Data/portfolio.csv', 'Data/stocklog.csv', 'txt')
